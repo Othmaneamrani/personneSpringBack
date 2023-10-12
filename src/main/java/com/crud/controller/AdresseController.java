d5d13f66ec2d31e076d7d3732c98b47605b372e5
othmaneamrani <othmaneamrani31@gmail.com>
2023-10-12 23:12:46 +0100

    cors adresse

--- src/main/java/com/crud/controller/AdresseController.java
+++ src/main/java/com/crud/controller/AdresseController.java
@@ -2,6 +2,7 @@ package com.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.crud.service.IAdresseService;
 
 @RestController
 @RequestMapping("/adresses") 
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdresseController {
 	
 	@Autowired 
